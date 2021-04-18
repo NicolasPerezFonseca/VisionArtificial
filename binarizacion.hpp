9fdd7dc755eb4ba8e68f6dce4be8690421c6fa50
NicolasPerezFonseca <n_perez@javeriana.edu.co>
2021-04-18 15:09:01 -0500

    Binarización implementada

--- binarizacion.hpp
+++ binarizacion.hpp
@@ -6,10 +6,31 @@
 using namespace cv;
 using namespace std;
 
+
+Mat erosion(Mat imagen, int tam)
+{
+  cv::Size size(imagen.cols,imagen.rows);
+  Mat element = getStructuringElement( MORPH_ELLIPSE , Size( 2*tam + 1, 2*tam+1 ),Point( tam, tam));
+  Mat destino = Mat::zeros(size, CV_64F);
+  erode( imagen, destino, element );
+  return destino;
+}
+
+Mat dilatacion(Mat imagen, int tam)
+{
+  cv::Size size(imagen.cols,imagen.rows);
+  Mat element = getStructuringElement( MORPH_ELLIPSE , Size( 2*tam + 1, 2*tam+1 ),Point( tam, tam));
+  Mat destino = Mat::zeros(size, CV_64F);
+  dilate( imagen, destino, element );
+  return destino;
+}
+
 Mat binarizacion(Mat imagen){
-    Mat imgGray,imgBlur,imgCanny,imageErode;
+    Mat imgGray,imgBlur,imgCanny,imageErode,imgDia;
     cvtColor(imagen,imgGray,COLOR_BGR2GRAY);
-    GaussianBlur(imagen,imgBlur,Size(3,3),3,0);
+    GaussianBlur(imgGray,imgBlur,Size(3,3),3,0);
     Canny(imgBlur,imgCanny,35,75);
-    return imgCanny;
+    imgDia = dilatacion(imgCanny,20);
+    imgDia =  erosion(imgDia,2);
+    return imgDia;
 }
