ae507c4f59d4b53e90f60c7a136cb46152b96d37
Ljara20 <luisjaramilloespinos16@gmail.com>
2021-05-30 20:52:42 -0500

    ya saca ecuaciones

--- linea.hpp
+++ linea.hpp
@@ -3,9 +3,57 @@
 #include <opencv2/highgui.hpp>
 
 
+class Ecuacion{
+    int x1,x2,y1,y2;
+    float m;
+    float b;
+    public:
+    Ecuacion(int x1,int x2, int y1,int y2){
+        this->x1=x1;
+        this->x2=x2;
+        this->y1=y1;
+        this->y2=y2;
+        this->m=((y2-y1)/(x2-x1));
+        this->b=y1-m*x1;
+    }
+    void interseccion(int *x, int *y,Ecuacion linea2){
+         (*x)=((linea2.b - this->b)/(this->m - linea2.m));
+         (*y)=(this->m*(*x)+this->b);
+    }
+
+};
+
+
+
 void dibujarlinea( Mat img, Point start, Point end )
 {
-  int thickness = 2;
+  int thickness = 10;
   int lineType = LINE_8;
   line( img,start,end,Scalar( 0, 0, 0 ),thickness,lineType );
+}
+list<Punto> extraerEsquinas(list<Punto> puntos){
+    //Point Punto1(puntos.front().j,puntos.front().i);
+    Punto inicial = puntos.front();
+    Punto finali = puntos.back();
+    list<Punto> retorno;
+    int diferencia = (finali.j-inicial.j)/2;
+    Punto min1=inicial, min2=finali; 
+    for (Punto p:puntos){
+        if(p.j<inicial.j+diferencia){
+            if(p.i<min1.i){
+                min1=p;
+            }
+        }
+        else{
+            if(p.i<min2.i){
+                min2=p;
+            }
+        }
+    }
+    retorno.push_back(inicial);
+    retorno.push_back(min1);
+    retorno.push_back(min2);
+    retorno.push_back(finali);
+    return retorno;
+
 }
--- proyecto_v1.cxx
+++ proyecto_v1.cxx
@@ -31,15 +31,34 @@ int main(int argc, char** argv)
   {
     cout<<"fila: "<< p.i<<" columna: "<<p.j<<endl;
   }
-  Point Punto1(puntos.front().j,puntos.front().i);
-  Point Punto2(puntos.back().j,puntos.back().i);
   
- 
+  
+
+  int x,y;
+  Punto p1=puntos.front();
+  puntos.pop_front();
+  Punto p2=puntos.front();
+  puntos.pop_front();
+  Punto p3=puntos.front();
+  puntos.pop_front();
+  Punto p4=puntos.front();
+  puntos.pop_front();
+
+  Ecuacion e1(p1.i,p2.i,p1.j,p2.j);
+  Ecuacion e2(p3.i,p4.i,p3.j,p4.j);
+  e1.interseccion(&x,&y,e2);
+  cout<< "x: "<<x<<" y: "<<y<<endl;
+
+  Point Punto1(p1.j,p1.i);
+  Point Punto2(y,x);
   dibujarlinea(imagen,Punto1,Punto2);
   escribirImagen(imagen,"lineas");
+  
   return( 0 );
   
 }
+
+
 /* To DO 
  1. Calcular las scuacionesd de las lineas para econtrar el horizonte y=mx+b
  2. DIbujar la Linea 
